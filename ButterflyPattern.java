a48cd93e38f584dfd9903712d28e6ab34c21b3ba
Simanto Saha <sahasimanto.828@gamil.com>
2025-02-21 11:13:51 +0600

    code by simanto saha

---
+++ ButterflyPattern.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ButterflyPattern {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your wished number : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int space = 2 * (n - i);
+            for (int k = 1; k <= space; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int space = 2 * (n - i);
+            for (int k = 1; k <= space; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
